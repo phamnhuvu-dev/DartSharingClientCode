7c2ff704f5647c56ed3a6169cdc024643281a6fd
Phạm Như Vũ <phamnhuvu.dev@gmail.com>
2018-12-23 21:51:10 +0700

    [App] add validtor module

--- app/lib/src/modules/validator.dart
+++ app/lib/src/modules/validator.dart
@@ -1,13 +1,19 @@
 class Validator {
-  validEmail(String email) {
-
+  bool validEmail(String email) {
+    RegExp emailRegExp = new RegExp(r"[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+        "\\@" +
+        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+        "(" +
+        "\\." +
+        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+        ")+");
+    final strMatch = emailRegExp.stringMatch(email);
+    return strMatch == email;
   }
 
-  validName(String text) {
-
-  }
-
-  validPassword() {
-
+  validAccountName(String accountName) {
+    RegExp nameRegExp = new RegExp(r"^[_A-z0-9]*((-|)*[_A-z0-9])*$");
+    final strMatch = nameRegExp.stringMatch(accountName);
+    return strMatch == accountName;
   }
 }
